a5a2c61fba75073c5b79f55fdd59ca39bf989871
XiShuiChen <shuichenxi@gmail.com>
2024-03-06 14:38:48 +0800

    初始项目创建

---
+++ springboot-template-backend/src/main/java/com/example/SpringbootTemplateBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTemplateBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootTemplateBackendApplication.class, args);
+    }
+
+}
