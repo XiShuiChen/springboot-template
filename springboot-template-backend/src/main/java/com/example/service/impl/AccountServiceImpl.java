f219e30e6b9e59edbb4cb6f04c8064529e4033d5
XiShuiChen <shuichenxi@gmail.com>
2024-03-11 21:39:22 +0800

    使用消息队列实现验证码的发送，并实现限流功能，防止用户频繁请求接口；完善注册操作，包括对用户名的正则校验、使用mp-plus将用户存到数据库等

--- springboot-template-backend/src/main/java/com/example/service/impl/AccountServiceImpl.java
+++ springboot-template-backend/src/main/java/com/example/service/impl/AccountServiceImpl.java
@@ -1,22 +1,95 @@
 package com.example.service.impl;
 
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.example.entity.dto.Account;
+import com.example.entity.vo.request.EmailRegisterVO;
 import com.example.mapper.AccountMapper;
 import com.example.service.AccountService;
+import com.example.utils.Const;
+import com.example.utils.FlowUtils;
+import jakarta.annotation.Resource;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-@Service
-public class AccountServiceImpl extends ServiceImpl<AccountMapper, Account> implements AccountService{
+import java.util.Date;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
-    public Account findAccountByNameOrEmail(String text){
+@Service
+public class AccountServiceImpl extends ServiceImpl<AccountMapper, Account> implements AccountService {
+
+    @Resource
+    AmqpTemplate amqpTemplate;
+
+    @Resource
+    StringRedisTemplate stringRedisTemplate;
+
+    @Resource
+    FlowUtils utils;
+
+    @Resource
+    PasswordEncoder encoder;
+
+    public Account findAccountByNameOrEmail(String text) {
         return this.query()
                 .eq("username", text).or()
                 .eq("email", text)
                 .one();
+    }
+
+    @Override
+    public String registerEmailVerifyCode(String type, String email, String ip) {
+        synchronized (ip.intern()) {
+            if (!this.verifyLimit(ip)) return "请求频繁，请稍后再试！";
+            Random random = new Random();
+            int code = random.nextInt(899999) + 100000;
+            Map<String, Object> data = Map.of("type", type, "email", email, "code", code);
+            amqpTemplate.convertAndSend("mail", data);
+            stringRedisTemplate.opsForValue()
+                    .set(Const.VERIFY_EMAIL_DATA + email, String.valueOf(code), 3, TimeUnit.MINUTES);
+            return null;
+        }
+    }
+
+    @Override
+    public String registerEmailAccount(EmailRegisterVO vo) {
+        String email = vo.getEmail();
+        String username = vo.getUsername();
+        String key = Const.VERIFY_EMAIL_DATA + email;
+        String code = stringRedisTemplate.opsForValue().get(key);
+        if (code == null) return "请先获取验证码";
+        if (!code.equals(vo.getCode())) return "验证码输入错误，请重新输入";
+        if (this.exitsAccountByEmail(email)) return "此电子邮件已被其他用户注册";
+        if (this.exitsAccountByUsername(username)) return "此用户名已被其他用户注册";
+        String password = encoder.encode(vo.getPassword());
+        Account account = new Account(null, username, password, email, "user", new Date());
+        if (this.save(account)) {
+            stringRedisTemplate.delete(key);
+            return null;
+        } else {
+            return "发生了内部错误，请联系管理员";
+        }
+    }
+
+    private boolean exitsAccountByEmail(String email) {
+        return this.baseMapper.exists(Wrappers.<Account>query().eq("email", email));
+    }
+
+    private boolean exitsAccountByUsername(String username) {
+        return this.baseMapper.exists(Wrappers.<Account>query().eq("username", username));
+    }
+
+    private boolean verifyLimit(String ip) {
+        String key = Const.VERIFY_EMAIL_LIMIT + ip;
+        return utils.limitOnceCheck(key, 60);
     }
 
     @Override
